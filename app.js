56b19b00c2f9002567b5ea393d805b82c0c35dd6
hritik pandey <128885231+hri1234@users.noreply.github.com>
2023-11-10 23:46:22 +0530

    Update app.js
    3rd commit

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ let allowedOrigins = [
   "https://pososf.onrender.com",
 "https://pososf.netlify.app",
   "http://localhost:3001",
-  " https://pos-os-frontend-n43d2tirz-hritik-4642d12e.vercel.app"
+  "https://pos-os-frontend-n43d2tirz-hritik-4642d12e.vercel.app"
 
 ];
 
